corrected calculation of cluster characteristics, updated requirements

clustering_statistics.py
@@ -15,27 +15,24 @@ def get_cluster_statistics(words: pd.DataFrame) -> dict:
     # count cluster occurences
     cluster_size_dict = {}
     cluster_size_list = []
-    cluster_curr_id = np.NAN
+    cluster_curr_id = -1
     cluster_curr_count = 0
 
     words = words.copy().reset_index(drop=True)
 
     for i in range(0, words.shape[0] + 1):
         # store clusters at end of a cluster (-> at last position or if cluster id changes)
-        if (i == words.shape[0]) or (cluster_curr_id != words.loc[i]["cluster"]):
-            # store the cluster if size is > 1
-            if cluster_curr_count > 1:
-                if cluster_curr_count not in cluster_size_dict.keys():
-                    cluster_size_dict[cluster_curr_count] = 0
-                cluster_size_dict[cluster_curr_count] += 1
-                cluster_size_list.append(cluster_curr_count)
+        if (i > 0) and ((i == words.shape[0]) or (cluster_curr_id != words.loc[i]["cluster"])):
+            # store the cluster always
+            if (cluster_curr_count - 1) not in cluster_size_dict.keys():
+                cluster_size_dict[cluster_curr_count - 1] = 0
+            cluster_size_dict[cluster_curr_count - 1] += 1
+            cluster_size_list.append(cluster_curr_count - 1)
             # reset cluster counter
             cluster_curr_count = 0
 
-        # increase cluster size count if 1) there is a cluster (!= np.NAN) the first condition only prevents an error
-        # with the index when calculating the latter conditions
-        if (i < words.shape[0]) and not np.isnan(words.loc[i]["cluster"]):
-            cluster_curr_count += 1
+        # increase cluster size count
+        cluster_curr_count += 1
 
         if i < words.shape[0]:
             cluster_curr_id = words.loc[i]["cluster"]
@@ -54,5 +51,6 @@ def get_cluster_statistics(words: pd.DataFrame) -> dict:
     return {"mean": mean_size,
             "median": median_size,
             "max": max_size,
-            "number_of_clusters": cluster_count,
+            "count": max(0, cluster_count - 1), # number of switches, minimum 0
             "count_per_size": cluster_size_dict}
+

clustering_traditional.py
@@ -147,6 +147,48 @@ class TraditionalClustering:
         categories2 = self.semantic_categories[self.semantic_categories["word"] == word2]["category"].values
         return np.intersect1d(categories1, categories2)
 
+    def get_commonrules_from_wordpair(self, word1: str, word2: str, printwarning: bool = True) -> set:
+        """
+        Returns all fullfilled phonematic rules of a word pair
+        :param word1: the first word
+        :param word2: the second word
+        :param printwarning: bool, whether to print a warning if one word is not found in the phonematic pair list
+        :return: set of all fullfilled rules
+        """
+
+        word1 = word1.lower()
+        word2 = word2.lower()
+
+        # sort words
+        if word1 > word2:
+            tmp = word1
+            word1 = word2
+            word2 = tmp
+
+        # check if words do exist
+        if printwarning:
+            notfound = self.check_wordlist_phonematic(pd.Series([word1, word2]))
+            if notfound.shape[0] > 0:
+                print("WARNING: words not found in phonemic pair list: ")
+                print(notfound)
+
+        # check if words are a pair
+        search_res = self.phonematic_pairs.loc[(self.phonematic_pairs["word1"] == word1) &
+                                               (self.phonematic_pairs["word2"] == word2)]
+        # pair not found
+        if search_res.shape[0] != 1:
+            if printwarning:
+                print("WARNING: word pair is not in phonemic pair list: ")
+            return {}
+
+        # else check which rules are fullfilled
+        shared_rules = []
+        for rule in ["first_two", "rhyme", "vowel_diff_only", "homonyms"]:
+            if search_res[rule].values[0] == 1:
+                shared_rules.append(rule)
+
+        return set(shared_rules)
+
     def check_same_category(self, word1: str, word2: str, printwarning: bool = True) -> bool:
         """
         Checks if both words are part of any same semantic category
@@ -243,10 +285,12 @@ class TraditionalClustering:
         :return: the given pd.DataFrame with 2 additional columns: cluster (indicating the cluster ID) +
         category_before (indicating the categories shared with the row before)
         """
-        cluster_id = 0
         intervals.reset_index(drop=True, inplace=True)
         intervals["cluster"] = np.NAN
         intervals["category_before"] = ""
+
+        cluster_id = 0
+        curr_cluster_lists = {}
 
         # if printwartning=True -> check if all words exist
         if printwarning:
@@ -255,25 +299,17 @@ class TraditionalClustering:
                 if printwarning and word not in self.semantic_categories["word"].values and word != "":
                     print("WARNING: " + word + " does not exist in semantic category list")
 
-        for i in range(0, intervals.shape[0] - 1):
-            is_cluster = self.check_same_category(intervals.loc[i, "word"], intervals.loc[i + 1, "word"],
-                                                  printwarning=False)
-            same_categories = self.get_categories_from_wordpair(intervals.loc[i + 1, "word"],
-                                                                intervals.loc[i, "word"], printwarning=False)
+        for i in range(0, intervals.shape[0]):
+            curr_lists = self.get_categories_from_word(intervals.loc[i, "word"])
 
-            if is_cluster:
-                intervals.loc[i + 1, "category_before"] = ",".join(same_categories)
-                if i == 0:
-                    cluster_id += 1  # new cluster
-                    intervals.loc[i, "cluster"] = cluster_id  # always a new cluster
-                    intervals.loc[i + 1, "cluster"] = cluster_id
-                else:
-                    if not np.isnan(intervals.loc[i, "cluster"]):
-                        intervals.loc[i + 1, "cluster"] = intervals.loc[i, "cluster"]  # same cluster as last element
-                    else:
-                        cluster_id += 1  # new cluster
-                        intervals.loc[i, "cluster"] = cluster_id
-                        intervals.loc[i + 1, "cluster"] = cluster_id
+            if len(set(curr_lists).intersection(curr_cluster_lists)) > 0:
+                curr_cluster_lists = set(curr_lists).intersection(curr_cluster_lists)
+                intervals.loc[i, "category_before"] = ", ".join(curr_cluster_lists)
+            else:
+                curr_cluster_lists = curr_lists
+                cluster_id += 1
+
+            intervals.loc[i, "cluster"] = cluster_id
 
         return intervals
 
@@ -286,9 +322,10 @@ class TraditionalClustering:
         :param intervals: pd.DataFrame with column 'words'
         :return: the given pd.DataFrame with 1 additional column: cluster (indicating the cluster ID)
         """
+
         intervals.reset_index(drop=True, inplace=True)
-        cluster_id = 0
         intervals["cluster"] = np.NAN
+        intervals["rule_before"] = ""
 
         # if printwartning=True -> check if all words exist
         if printwarning:
@@ -297,21 +334,27 @@ class TraditionalClustering:
                 print("WARNING: words do not exist in phonemic pair list: ")
                 print(not_found)
 
-        for i in range(0, intervals.shape[0] - 1):
-            is_cluster = self.check_phonemic_pair(intervals.loc[i, "word"], intervals.loc[i + 1, "word"],
-                                                  printwarning=False)
+        cluster_id = 0
+        curr_cluster_lists = {}
+        intervals.loc[0, "cluster"] = cluster_id
 
-            if is_cluster:
-                if i == 0:
-                    cluster_id += 1  # new cluster
-                    intervals.loc[i, "cluster"] = cluster_id  # always a new cluster
-                    intervals.loc[i + 1, "cluster"] = cluster_id
-                else:
-                    if not np.isnan(intervals.loc[i, "cluster"]):
-                        intervals.loc[i + 1, "cluster"] = intervals.loc[i, "cluster"]  # same cluster as last element
+        for i in range(1, intervals.shape[0]):
+            curr_lists = self.get_commonrules_from_wordpair(intervals.loc[i - 1, "word"], intervals.loc[i, "word"])
+
+            if len(curr_lists) == 0:
+                cluster_id +=1
+                curr_cluster_lists = {}
+            else:
+                if len(curr_cluster_lists) > 0:
+                    if len(curr_lists.intersection(curr_cluster_lists)) > 0:
+                        curr_cluster_lists = curr_lists.intersection(curr_cluster_lists)
                     else:
-                        cluster_id += 1  # new cluster
-                        intervals.loc[i, "cluster"] = cluster_id
-                        intervals.loc[i + 1, "cluster"] = cluster_id
+                        cluster_id += 1
+                        curr_cluster_lists = {}
+                else:
+                    curr_cluster_lists = curr_lists
+
+            intervals.loc[i, "rule_before"] = ", ".join(curr_cluster_lists)
+            intervals.loc[i, "cluster"] = cluster_id
 
         return intervals

clustering_word2vec.py
@@ -148,10 +148,10 @@ class Word2VecModel:
         algorithm is used, this value will we ignored
         :param clustering_type: used clustering-mechanism (possible values are: 'fixed_chain', 'fixed_cluster',
         'dynamic_chain', 'dynamic_cluster') See Linz et. al 2017 for details.
-        :return: the given pd.DataFrame with 2 additional columns: cluster (indicating the cluster ID), rel_before
+        :return: the given pd.DataFrame with 1 additional column: cluster (indicating the cluster ID)
         """
         cluster_ids = [np.NAN for _ in intervals.index]
-        rels = [np.NAN for _ in intervals.index]
+        cluster_ids[0] = 0
         cluster_id = 0
 
         intervals.reset_index(drop=True, inplace=True)
@@ -168,34 +168,24 @@ class Word2VecModel:
         # else:
             # print("using fixed similarity threshold " + str(sim_threshold) + " for " + clustering_type)
 
-        for i in range(0, intervals.shape[0] - 1):
+        for i in range(1, intervals.shape[0]):
 
             if clustering_type == "dynamic_chain" or clustering_type == "fixed_chain":
                 try:
-                    pairwise_relatedness = self.cosine_similarity(intervals.loc[i, "word"],
-                                                        intervals.loc[i + 1, "word"])
-                    rels[i+1] = pairwise_relatedness
-                    is_cluster = pairwise_relatedness > sim_threshold
+                    is_cluster = self.cosine_similarity(intervals.loc[i, "word"],
+                                                        intervals.loc[i - 1, "word"]) > sim_threshold
                 except KeyError:
                     is_cluster = False
-                    print("WARNING: word pair not found: " + intervals.loc[i, "word"] + " - " + intervals.loc[i + 1, "word"])
+                    print("WARNING: word pair not found: " + intervals.loc[i, "word"] + " - " + intervals.loc[i - 1, "word"])
             else:
                 raise NotImplementedError("clusterin_type " + clustering_type + " not implemented yet!")
 
-            if is_cluster:
-                if i == 0:
-                    cluster_id += 1  # new cluster
-                    cluster_ids[i] = cluster_id  # always a new cluster
-                    cluster_ids[i + 1] = cluster_id
-                else:
-                    if not np.isnan(cluster_ids[i]):
-                        cluster_ids[i + 1] = cluster_ids[i]  # same cluster as last element
-                    else:
-                        cluster_id += 1  # new cluster
-                        cluster_ids[i] = cluster_id
-                        cluster_ids[i + 1] = cluster_id
+            if not is_cluster:
+                cluster_id += 1
 
-        return cluster_ids, rels
+            cluster_ids[i] = cluster_id
+
+        return cluster_ids
 
     def calculate_dynamic_threshold(self, intervals: pd.DataFrame) -> float:
         """